5508e942f376259bf32814861acb9b325f86e438
pauEscarcia <paulina_151090@hotmail.com>
2017-03-06 00:29:57 -0600

    tanks

---
+++ formulario3.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+		<meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+        <title>Análisis de Software en Epic Queen</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="description" content="Análisis de Software en Epic Queen" />
+        <meta name="keywords" content="epic queen, analisis, software, niñas" />
+        <meta name="author" content="Pau Escarcia" />
+        <link rel="shortcut icon" href="../favicon.ico"> 
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700,300,300italic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="css/style4.css" />
+		<script src="js/modernizr.custom.72111.js"></script>
+		<style>
+			.no-cssanimations .rw-wrapper .rw-sentence span:first-child{
+				opacity: 1;
+			}
+		</style>
+		<!--[if lt IE 9]> 
+			<style>
+				body{ background: #fff; }
+				.rw-wrapper{ display: none; } 
+				.rw-sentence-IE{ display: block;  }
+			</style>
+		<![endif]-->
+    </head>
+    <body>
+        <div class="container">
+			<!-- Codrops top bar -->
+            <div class="codrops-top">
+              
+               	
+                <div class="clr"></div>
+            </div><!--/ Codrops top bar -->
+			<header>
+					<h1><strong>Formulario de Análisis de Software </strong>en Epic Queen</h1>
+				
+			</header>
+			<section class="rw-wrapper">
+				<h2 class="rw-sentence">
+					<span>En Epic Queen &amp; CIMAT</span>
+					<div class="rw-words rw-words-1">
+						<span>Agradecemos</span>
+						<span>Estamos</span>
+						<span>Para empoderar  </span>
+						<span>En áreas</span>
+						
+					</div>
+					<div class="rw-words rw-words-2">
+						<span>su tiempo y atención</span>
+						<span>evaluando el mejor software</span>
+						<span>a más mujeres y niñas</span>
+						<span>de ciencia y tecnología</span>
+					</div>
+				</h2>
+			</section>
+			
+        </div>
+    </body>
+</html>
